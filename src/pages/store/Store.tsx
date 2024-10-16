651033033aa040f5197b4906df256bef67a895d1
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2024-10-17 00:01:20 +0330

    Make api.ts to make function for call API and use it in store page to get data

--- src/pages/store/Store.tsx
+++ src/pages/store/Store.tsx
@@ -1,38 +1,95 @@
 // libraries
-import React from 'react'
+import { useEffect, useState } from "react";
 
 // components
-import Container from '../../components/container/Container'
-import ProductItem from '../../components/productItem/ProductItem'
-import { Link } from 'react-router-dom'
+import Container from "../../components/container/Container";
+import ProductItem from "../../components/productItem/ProductItem";
+import { Link } from "react-router-dom";
+
+// API call function
+import { getProductsAPI } from "../../services/api";
 
 function Store() {
+  const [productsData, setProductsData] = useState([]);
+
+  /*
+    ma inja miaim az 'useEffect' estefade mikonim ta az charkhe zendegi 'lifecycle React' estefade konim va API call anjam bedim.
+    
+    alan 'getProductsAPI' yek 'async function' ast ke yani yek 'promise' barmigardune yani ta zamani ke data kamel daryaft nashe
+    'return' nemikone pas ma miaim az 'then' estefade mikonim.
+
+    dar asl 'then' miad az natije nahayi 'zamani ke promise takmil shod' estefade mikone; amalan montazer mimune ta API call kamel 
+    beshe va data 'return' shode ro dar 'productsAPI' barmigardune va function dakhelesh ro ejra mikone.
+
+
+  */
+
+  useEffect(() => {
+    getProductsAPI().then((productsAPI) => {
+      /*
+        console.log(productsAPI);
+        "
+          Array(5) [
+            0: {id: '1', name: 'محصول اول', price: 1000000, description: 'این یک متن تستی برای نشان دادن اینکه این یک محصول … این یک محصول است که در یک کامپوننت جدا قرار داره', imageUrl: 'https://static.vecteezy.com/system/resources/previ…in-supermarket-illustration-for-banner-vector.jpg'}
+            1: {id: '2', name: 'محصول دوم', price: 1000000, description: 'این یک متن تستی برای نشان دادن اینکه این یک محصول … این یک محصول است که در یک کامپوننت جدا قرار داره', imageUrl: 'https://static.vecteezy.com/system/resources/previ…in-supermarket-illustration-for-banner-vector.jpg'}
+            2: {id: '3', name: 'محصول سوم', price: 1000000, description: 'این یک متن تستی برای نشان دادن اینکه این یک محصول … این یک محصول است که در یک کامپوننت جدا قرار داره', imageUrl: 'https://static.vecteezy.com/system/resources/previ…in-supermarket-illustration-for-banner-vector.jpg'}
+            3: {id: '4', name: 'محصول چهارم', price: 1000000, description: 'این یک متن تستی برای نشان دادن اینکه این یک محصول … این یک محصول است که در یک کامپوننت جدا قرار داره', imageUrl: 'https://static.vecteezy.com/system/resources/previ…in-supermarket-illustration-for-banner-vector.jpg'}
+            4: {id: '5', name: 'محصول پنجم', price: 1000000, description: 'این یک متن تستی برای نشان دادن اینکه این یک محصول … این یک محصول است که در یک کامپوننت جدا قرار داره', imageUrl: 'https://static.vecteezy.com/system/resources/previ…in-supermarket-illustration-for-banner-vector.jpg'}
+          ]
+        "
+      */
+
+      /*
+        vaghti data az API call bar migardan dar inja data ro be 'productsAPI' ekhtesas mide va badesh 'function setProductsData' 
+        farakhani mikone ke marbut be yek 'state' ast; in function data ro dakhel 'state productsData' gharar mide va component ro 
+        'rerender' mikone ta data namayesh bede. 
+      */
+      setProductsData(productsAPI);
+    });
+  }, []);
+
   return (
     <Container>
-      <h1 className="text-xl text-right my-4">محوصلات</h1>
+      <h1 className="text-xl text-right my-4">محصولات</h1>
       <div className="grid grid-cols-4 gap-5 mt-3">
-        <Link to={`/product/${1}`} >
-          <ProductItem />
-        </Link>
-        <Link to={`/product/${2}`} >
-          <ProductItem />
-        </Link>
-        <Link to={`/product/${3}`} >
-          <ProductItem />
-        </Link>
-        <Link to={`/product/${4}`} >
-          <ProductItem />
-        </Link>
-        <Link to={`/product/${5}`} >
-          <ProductItem />
-        </Link>
-        <Link to={`/product/${6}`} >
-          <ProductItem />
-        </Link>
-      </div>
+        {/* 
+          ma az 'map' dar React estefade mikonim ta betunim 'araye ya list az data' ro be yek 'araye ya list az anasor JSX' tabdil konim
+          va har onsor ro dar 'rabet karbari ya UI' namayesh bedim.
 
+          dar inja 'araye ya list az data' dakhel 'state productsData' hast; har product be onvan 'object' dar in list vojud dare va ma 
+          bayad har product ro dar ghaleb 'ProductItem component' render konim. 
+
+          hala dalayel estefade az function 'map':
+
+          1- tabdil araye be anasor JSX: 
+            function 'map' be ma in ejaze ro mide ke har 'object' az 'state productsData' ro be yek onsor JSX tabdil mikone; dar in kar
+            har product be yek 'Link az react-router-dom' va yek 'ProductItem component' tabdil mishe.
+          
+          2- modiriat taghyirat va React: 
+            vaghti az function 'map' estefade mikonim va be har onsor JSX 'key' ekhtesas bedim React mitune be surat behine motevajeh 
+            beshe ke che taghyirat dar araye 'productsData' etefagh oftade va faghat oun anasor ro ke taghyir kardan 'rerender' mikone
+            ke render sari tar va behine tar mishe.
+
+          hala nokte mohem inja ine ke aslan ma chera bejaye 'map' az 'for' ya 'foreach' estefade nemikonim?
+
+          vaghti az 'map' estefade mikonim miad baraye har 'item ya object' dar 'araye ya list' yek 'onsor ya tag JSX' tabdil mikone.
+          hala miad in tag ro 'return' mikone. ama dar asl miad baraye har 'tag JSX' ke misaze dakhel yek 'araye ya list' jadidi ke
+          misaze gharar mide ke shamel majmueyi az 'tag JSX' hast va React ba didan mohtaviat in 'araye' jadid rerender anjam mide.
+
+          ama 'foreach' baraye ejra yek 'item' dar function estefade mishe va hich meghdari 'return' nemikone. baraye hamin az 'foreach'
+          baraye 'render' list 'tag JSX' monaseb nist chon majburim yek araye be surat dasti besazim va tag JSX ezafe konim.
+
+          ama 'for' ye halghe control hast ke be surat mostaghim meghdari ro 'return' nemikone.
+
+        */}
+        {productsData.map((product) => (
+          <Link to={`/product/${product}`}>
+            <ProductItem />
+          </Link>
+        ))}
+      </div>
     </Container>
-  )
+  );
 }
 
-export default Store
+export default Store;
---
+++ src/services/api.ts
@@ -0,0 +1,69 @@
+// libraries
+import axios from "axios";
+
+/*
+  dar inja ma mikhaim function besazim ke biad data ro ba estefade az 'axios' begirim va harja ke bekhaim ba seda zadan function data
+  ro begirim in kar baraye tamizi va chandbar estefade kardan code ast.
+
+  ma miaim ye moteghayer 'client' misazim ke tush miakhaim 'config' baraye 'axios' moshakhas konim. yani amalan ba zadan 'axios.create' 
+  ma nemune sefareshi az 'axios' misazim ke in ejaze ro mide tanzimat pishfarz baraye darkhast ha gharar bedim.
+
+  miaim migim 'baseUrl' ke dakhel url mishe oun tike az link ke sabete masalan: "http://localhost:8000/products" dar inja bakhsh 
+  "http://localhost:8000" sabete va tike akhar taghyir mikone pas baraye in ke yek bar url ro moshakhas konim va age ruzi 
+  back-end kar oumad taghyir dad ma faghat yek ja taghyir midim va niazi nist har bar dasti in kar ro konim.
+
+  hala har bar az 'client' estefade konim masalan 'client("/products")' inja 'axios' khod be khod miad darkhast be url 
+  "http://localhost:8000/products" ersal mikone.
+
+  vaghti miaim 'client' misazim 'axios' miad be surat default darkhast ro 'GET' migire ta zamani ke darkhast ro moshakhas konim.
+*/
+
+const client = axios.create({
+  baseURL: "http://localhost:8000",
+});
+
+/*
+  dar inja ma mikhaim 'async function' besazim ke biad data ro az API begire. vaghti ye function ro ba 'async' tarif mikonim behet
+  ejaze mide az 'await' estefade konim. dar asl 'await' sabr mikone ye 'vade ya promise' takmil beshe va badesh edame code ro ejra mikone.
+  in kar zamani mofide ke 'API call' be khater kondi internet ya server momkene tul bekeshe va nakhaim code haye badish ejra beshe.
+  
+  hala ma miaim migim in data momkene dir biad ke dalilesh shayad kondi internet ya shayad bad code zadan back-end va dar javascript 
+  code az bala be tartib ejra mishe miad payin va zamani ke data dir mirese va code badi ejra mishe momkene ghabl residan data code ma 
+  error bokhore pas bayad biaim begim zamani ke data ro mikhai begiri vaisa ta data biad badesh boro code badi ro ejra bokon ke in 
+  kar ba estefade az 'async await' etefagh miofte.
+
+  pas fahmidim ke "await client("/products");" in code mige ta zamani ke data az url khaste shode biad sabr kon badesh bia data ro 
+  'return' kon.
+*/
+
+export async function getProductsAPI() {
+  /*
+    shoma zamani ke "const {data} = await client("/products");" az in code estefade mikonim yani az chizi be esm 'destructuring'
+    estefade kardim. in be ma ejaze mide be tor mostaghim az darun object ke dar inja pasokh 'axios' ast; faghat oun chizi ro ke mikhaim
+    estekhraj mikonim yani amalan az kol object faghat oun tike ke khastim ro bar midarim.
+
+    dar inja pasokh 'axios' ye object masalan: 
+    "
+      {
+        data: [...] // داده‌هایی که میخوای،
+        status: 200,
+        headers: {...}
+      }
+    "
+    hala vaghti az '{data}' estefade mikonim ke behesh 'destructuring' migan faghat bakhsh 'data' to migire va niazi nist be surat
+    'response.data' behesh dastresi dasht.
+
+    ama vaghti "const data = await client("/products");" minevisi dare kol object ro dakhel 'data' ekhtesas mide va baraye dastresi
+    be data dakhel object bayad az 'data.data' estefade konim.
+
+  */
+
+  // ba estefade az destructuring
+  const { data } = await client("/products");
+  return data;
+
+  // bedun destructuring
+  // const data = await client("/products");
+  // return data.data;
+}
+
